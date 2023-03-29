a7fd87b180c7b50d43dc31333adebc508a695e8f
CarlosPisco <edupiscote.cept@gmail.com>
2023-03-29 16:31:57 -0500

    error carpeta

--- src/main/java/com/example/ramas/Controllers/CarController.java
+++ src/main/java/com/example/ramas/Controllers/CarController.java
@@ -1,6 +1,7 @@
 package com.example.ramas.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CarController {
 
 
-
+    @GetMapping("/index")
+    public String vistaPrincipal(){
+        return "cars";
+    }
 
 }
