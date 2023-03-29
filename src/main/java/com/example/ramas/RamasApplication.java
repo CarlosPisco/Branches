f17724b4c4aac8d352b5633190342edff80c6325
CarlosPisco <edupiscote.cept@gmail.com>
2023-03-29 16:24:51 -0500

    first commit

---
+++ src/main/java/com/example/ramas/RamasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ramas/RamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
